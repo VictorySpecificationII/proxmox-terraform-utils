c1573892581a30ade9b719dc4e4c2c85ff037501
Antreas Christofi <VictorySpecificationII@users.noreply.github.com>
2022-08-13 17:16:36 +0300

    Update template-creation.sh

--- template-creation.sh
+++ template-creation.sh
@@ -8,6 +8,7 @@ rm focal-server-cloudimg-amd64.img
 wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
 virt-customize -a focal-server-cloudimg-amd64.img --install qemu-guest-agent
 virt-customize -a focal-server-cloudimg-amd64.img --password ubuntu:ubuntu
+virt-customize -a focal-server-cloudimg-amd64.img --root-password ubuntu
 qm create 9000 --name "ubuntu-2004-cloudinit" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
 qm importdisk 9000 focal-server-cloudimg-amd64.img local-lvm
 qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
