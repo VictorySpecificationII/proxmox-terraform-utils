7b551c7f20051e945d7e07a11a07a61e782f0a3f
Antreas Christofi <VictorySpecificationII@users.noreply.github.com>
2022-08-20 16:18:55 +0300

    Update template-creation.sh
    separate create user and ssh inject commands

--- template-creation.sh
+++ template-creation.sh
@@ -6,9 +6,14 @@ apt install libguestfs-tools -y
 # remove existing image in case last execution did not complete successfully
 rm focal-server-cloudimg-amd64.img
 wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+
 virt-customize -a focal-server-cloudimg-amd64.img --install qemu-guest-agent
-virt-sysprep -a focal-server-cloudimg-amd64.img --run-command 'useradd $USER' --ssh-inject $USER:file:/home/$USER/.ssh/id_rsa.pub
+virt-sysprep -a focal-server-cloudimg-amd64.img --run-command 'useradd devops' 
+virt-sysprep -a focal-server-cloudimg-amd64.img --ssh-inject devops:file:/root/.ssh/id_rsa.pub
+
 virt-customize -a focal-server-cloudimg-amd64.img --root-password password:ubuntu
+
+
 qm create 9000 --name "ubuntu-2004-cloudinit" --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
 qm importdisk 9000 focal-server-cloudimg-amd64.img local-lvm
 qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
