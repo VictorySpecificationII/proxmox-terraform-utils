4ff629520475f9cb52e07edcda4c2f0d79aba23c
root <root@cpu-3.intellectualplayspace.local>
2022-10-01 12:23:35 +0300

    added node ib

--- infiniband-setup-node.sh
+++ infiniband-setup-node.sh
@@ -2,4 +2,4 @@
 
 apt-get install opensm
 apt-get install net-tools
-echo -e "mlx4_core\nmlx4_ib\nib_umad\nib_uverbs\nib_ipoib\nsvcrdma" >> /etc/modules
+echo -e "mlx4_core\nmlx4_ib\nib_umad\nib_uverbs\nib_ipoib\nxprtrdma" >> /etc/modules
